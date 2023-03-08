fa668c6748a4ea8678cf83e8a4c9d6a31ebbaff8
cade-th <cade.thorn9@gmail.com>
2023-03-07 19:34:53 -0700

    hi

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
 chmod +x run.sh
 
-cd out/build ./OLAS
+cd out/build
+
+./OLAS
--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,6 @@ const unsigned int SCR_HEIGHT = 600;
 int main()
 {
 
-    printf("hello");
     // glfw: initialize and configure
     // ------------------------------
     // Initialize GLFW
@@ -37,7 +36,6 @@ int main()
     
     
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-    printf("hello\n");
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
